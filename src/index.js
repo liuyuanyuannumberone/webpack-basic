89e569d368c6167b1010797f7130bd6212894d1d
liuyuanyuan <1445179059@qq.com>
2020-05-26 22:34:28 +0800

    webpack 图片和样式上

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-const Girl=require('./common/girl.jpg');
+import girl from './common/image/girl.jpg';
+import './common/css/index.css'
+import './common/css/main.scss'
+
+let img = new Image();
+img.src = girl;
+// img.classList.add("girl");
+img.classList.add("sc");
+var root = document.getElementById("root");
+root.append(img);
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,41 @@ module.exports = {
     module: {
         rules: [
             // {test: /\.(jpe?g|png|gif)$/, use: {loader: 'file-loader'}},
-            {test: /\.(jpe?g|png|gif)$/, loader: 'file-loader'},
+            // {
+            //     test: /\.(jpe?g|png|gif)$/,
+            //     loader: 'file-loader',
+            //     options: {
+            //         //占位符，webpack官网还有其他
+            //         name: '[name]_[hash].[ext]',
+            //         outputPath:'images/'
+            //     },
+            // },
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                loader: 'url-loader',
+                options: {
+                    //占位符，webpack官网还有其他
+                    name: '[name]_[hash].[ext]',
+                    outputPath:'images/',
+                    limit:20480 //（B 字节）默认将图片打包到js文件，但是图片很大时，超过20KB，
+                           // 执行file-loader的打包方式
+                },
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
         ]
     },
     output: {
