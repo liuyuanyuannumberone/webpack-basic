b12b4a99aa3e2b003648f1b6db382971a4818f7d
liuyuanyuan <1445179059@qq.com>
2020-05-27 23:08:18 +0800

    webpack 图片和样式上

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import girl from './common/image/girl.jpg';
 import './common/css/index.css'
+import style from './common/css/main.scss'  ////css模块化打包 这样引入样式文件
 import './common/css/main.scss'
 
 let img = new Image();
 img.src = girl;
-// img.classList.add("girl");
+//img.classList.add("girl");
+// img.classList.add(style.sc);
 img.classList.add("sc");
 var root = document.getElementById("root");
+root.innerHTML='<div class="iconfont icongongzuotai">abc</div>';
 root.append(img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -6,45 +7,53 @@ module.exports = {
     module: {
         rules: [
             // {test: /\.(jpe?g|png|gif)$/, use: {loader: 'file-loader'}},
-            // {
-            //     test: /\.(jpe?g|png|gif)$/,
-            //     loader: 'file-loader',
-            //     options: {
-            //         //占位符，webpack官网还有其他
-            //         name: '[name]_[hash].[ext]',
-            //         outputPath:'images/'
-            //     },
-            // },
+            //             {
+            //                 test: /\.(jpe?g|png|gif)$/,
+            //                 loader: 'file-loader',
+            //                 options: {
+            //                     //占位符，webpack官网还有其他
+            //                     name: '[name]_[hash].[ext]',
+            //                     outputPath:'images/'
+            //                 },
+            //             },
             {
                 test: /\.(jpe?g|png|gif)$/,
                 loader: 'url-loader',
                 options: {
                     //占位符，webpack官网还有其他
                     name: '[name]_[hash].[ext]',
-                    outputPath:'images/',
-                    limit:20480 //（B 字节）默认将图片打包到js文件，但是图片很大时，超过20KB，
-                           // 执行file-loader的打包方式
+                    outputPath: 'images/',
+                    limit: 20480 //（B 字节）默认将图片打包到js文件，但是图片很大时，超过20KB，
+                    // 执行file-loader的打包方式
                 },
             },
             {
                 test: /\.s[ac]ss$/,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
+                use: ['style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 2,
+                        // modules:true, //css模块化打包
+                    }
+                }, 'sass-loader', 'postcss-loader'],
+                //loader的执 行顺序从下往上，从右往左执行
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 2,
+                        // modules:true,
+                    }
+                }, 'postcss-loader'],
             },
-        ]
+            {test: /\.(eot|svg|ttf|woff|woff2)$/, use: {loader: 'file-loader'}},
+        ],
     },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({template: "src/index.html"})]
 };
